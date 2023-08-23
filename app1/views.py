c2ddf7d507c7f67ddf2446bce658b75d219eedd0
shafeeq797 <shafeeqtm97@gmail.com>
2023-08-23 17:43:48 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,17 @@
+
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
+from django.conf import settings
+from django.contrib.auth.models import User
+def index(request):
+    return render(request,'index.html')
+def create(request):
+    return render(request,'createaccount.html')
+def login(request):
+    return render(request,'login.html')
+def user(request):
+    return render(request,'user.html')
+
+
+# Create your views here.
