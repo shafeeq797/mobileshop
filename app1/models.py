d6a222881757e06b6582ef495a9d0e770a0458ff
shafeeq797 <shafeeqtm97@gmail.com>
2023-09-06 12:46:16 +0530

    table added

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+class useraccount_tbl(models.Model):
+    username=models.CharField(max_length=500)
+    firstname=models.CharField(max_length=500)
+    email=models.CharField(max_length=500)
+    phone=models.IntegerField()
+    accounttype=models.CharField(max_length=500)
+    class Meta:
+        db_table="useraccount_tbl"
+class user_tbl(models.Model):
+     username=models.CharField(max_length=500)
+     password=models.CharField(max_length=500)
+     firstname=models.CharField(max_length=500)
+     lastname=models.CharField(max_length=500)
+     gender=models.CharField(max_length=500)
+     email=models.CharField(max_length=500)
+     phone=models.IntegerField()
+     address=models.CharField(max_length=500)
+     district=models.CharField(max_length=500)
+     photo=models.CharField(max_length=1000)
+     class Meta:
+         db_table="user_tbl"
+class seller_tbl(models.Model):
+    firstname=models.CharField(max_length=500)
+    lastname=models.CharField(max_length=500)
+    username=models.CharField(max_length=500)
+    password=models.CharField(max_length=500)
+    gender=models.CharField(max_length=500)
+    email=models.CharField(max_length=500)
+    phone=models.IntegerField()
+    address=models.CharField(max_length=500)
+    district=models.CharField(max_length=500)
+    photo=models.CharField(max_length=1000)
+    class Meta:
+        db_table="seller_tbl"
+class staff_tbl(models.Model):
+    firstname=models.CharField(max_length=500)
+    lastname=models.CharField(max_length=500)
+    designation=models.CharField(max_length=500)
+    age=models.IntegerField()
+    gender=models.CharField(max_length=500)
+    email=models.CharField(max_length=500)
+    phone=models.IntegerField()
+    address=models.CharField(max_length=500)
+    district=models.CharField(max_length=500)
+    photo=models.CharField(max_length=500)
+    username=models.CharField(max_length=500)
+    password=models.CharField(max_length=500)
+    class Meta:
+        db_table="staff_tbl"
+
+
+    
+
+
 
 # Create your models here.
--- app1/views.py
+++ app1/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from django.contrib.auth.models import User
 def index(request):
     return render(request,'index.html')
+    # return render(request,'admin.html')
+
 def create(request):
     return render(request,'createaccount.html')
 def login(request):
